37d222defa615db1f6d3d636768e2852544efa8e
basandan <basandan@basandan-WX-1.corp.adobe.com>
2019-11-21 00:04:58 +0530

    added jira bug creation code

--- SeleniumJavaFramawork/src/test/java/test/GoogleSearch_TestNG.java
+++ SeleniumJavaFramawork/src/test/java/test/GoogleSearch_TestNG.java
@@ -7,11 +7,18 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import demo.Log4jDemo;
+import net.rcarz.jiraclient.BasicCredentials;
+import net.rcarz.jiraclient.Field;
+import net.rcarz.jiraclient.Issue;
+import net.rcarz.jiraclient.JiraClient;
+import net.rcarz.jiraclient.JiraException;
 
 public class GoogleSearch_TestNG {
 	WebDriver driver=null;
@@ -43,9 +50,9 @@ public class GoogleSearch_TestNG {
 		
 		}
 	@Test
-	public void testRetry1()
+	public void DivideByZero()
 	{
-		System.out.println("Inside testRetry1");
+		System.out.println("Inside  DivideByZero");
 		//int i=1/0;
 	}
 	@Test(retryAnalyzer=listeners.RetryAnalyzer.class)
@@ -53,6 +60,26 @@ public class GoogleSearch_TestNG {
 	{   count++;
 		System.out.println("Inside testRetry2");
 		Assert.assertTrue(2<=count);
+	}
+	@Test
+	public void newGoogleSearch()
+	{
+		driver.get("https://google.com");
+		Assert.assertEquals("Expected title", driver.getTitle());
+		
+	}
+	
+	@AfterMethod
+	public void afterMethod(ITestResult result) throws JiraException
+	{//If test case fails then log a ticket in jira
+		if(result.getStatus()==ITestResult.FAILURE)
+		{
+			BasicCredentials creds=new BasicCredentials("varshabasandani3996", "Testuser@123");
+			JiraClient jira=new JiraClient("http://localhost:8081/",creds);
+			Issue issueName=jira.createIssue("TES", "Bug").field(Field.SUMMARY, result.getMethod().getMethodName()+" is failed due to "+result.getThrowable().toString()).field(Field.DESCRIPTION, "Test Description").execute();
+		    System.out.println("Issue is created in JIRA with issue key :"+issueName.getKey());
+		}
+		
 	}
 	
 	@AfterTest
